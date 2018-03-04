52df744a32db6cbef1effa3f191f2d7ec1525690
Katie Poeltler <kpoeltler@gmail.com>
2018-03-03 20:06:06 -0600

    server

---
+++ menu-screen-test.js
@@ -0,0 +1,15 @@
+describe('image-resizer', function() {
+ 
+    it('should keep aspect ratio of landscape images if aspect ratio is enabled', function() {
+      //test code...
+    });
+   
+    it('should keep aspect ratio of horizontal images if aspect ratio is enabled', function() {
+      //test code...
+    });
+   
+    it('should compress image', function() {
+      //test code...
+    });
+   
+  });
