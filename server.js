6aa9d571906ea994fbeb4ff64e23c49bd4e5387b
Katie Poeltler <kpoeltler@gmail.com>
2018-03-03 20:28:04 -0600

    added npm installs

--- server.js
+++ server.js
@@ -35,3 +35,57 @@ require("./app/routing/html-routes.js")(app);
 app.listen(PORT, function() {
   console.log("App listening on PORT " + PORT);
 });
+
+
+//Parse data from JSON POST and insert into MYSQL
+
+var express = require('express');
+var app = express();
+var path = require('path');
+var bodyParser = require('body-parser');
+var mysql = require('mysql');
+
+// Configure MySQL connection
+var connection = mysql.createConnection({
+	host: 'localhost',
+	user: 'node',
+	password: 'node',
+	database: 'node_project'
+  })
+
+//Establish MySQL connection
+connection.connect(function(err) {
+   if (err) 
+      throw err
+   else {
+       console.log('Connected to MySQL');
+       // Start the app when connection is ready
+       app.listen(3000);
+       console.log('Server listening on port 3000');
+ }
+});
+
+app.use(bodyParser.json())
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname+ '/myfile.html'));
+});
+
+app.post('/', function(req, res) {
+
+var jsondata = req.body;
+var values = [];
+
+for(var i=0; i< jsondata.length; i++)
+  values.push([jsondata[i].name,jsondata[i].age]);
+
+//Bulk insert using nested array [ [a,b],[c,d] ] will be flattened to (a,b),(c,d)
+connection.query('INSERT INTO members (name, age) VALUES ?', [values], function(err,result) {
+  if(err) {
+     res.send('Error');
+  }
+ else {
+     res.send('Success');
+  }
+});
+});
